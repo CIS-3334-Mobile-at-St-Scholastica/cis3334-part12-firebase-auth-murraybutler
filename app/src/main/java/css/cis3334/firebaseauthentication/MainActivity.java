76123e24b739af3bcbb516530e776a1bd38c76a0
murraybutler <mbutler3@css.edu>
2018-04-14 13:25:55 -0500

    Changes for Unit 12

--- app/src/main/java/css/cis3334/firebaseauthentication/MainActivity.java
+++ app/src/main/java/css/cis3334/firebaseauthentication/MainActivity.java
@@ -4,12 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -26,6 +24,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+/**
+ * Class to test firebase auth methods
+ * @author murray.butler
+ * @version 1.0
+ * April 10, 2018
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewStatus;
@@ -35,12 +39,18 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonGoogleLogin;
     private Button buttonCreateLogin;
     private Button buttonSignOut;
+    private FirebaseAuth mAuth;
 
+    /**
+     * Standard onCreate() 0verride to setup env and vars
+     * @param savedInstanceState Bundle for state storage
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Declarations and associations
         textViewStatus = (TextView) findViewById(R.id.textViewStatus);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
@@ -48,31 +58,34 @@ public class MainActivity extends AppCompatActivity {
         buttonGoogleLogin = (Button) findViewById(R.id.buttonGoogleLogin);
         buttonCreateLogin = (Button) findViewById(R.id.buttonCreateLogin);
         buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        mAuth = FirebaseAuth.getInstance();
 
+        // Listener for click and action for normal signIn()
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("CIS3334", "normal login ");
                 signIn(editTextEmail.getText().toString(), editTextPassword.getText().toString());
             }
         });
 
+        // Listener for click and action for createAccount()
         buttonCreateLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("CIS3334", "Create Account ");
                 createAccount(editTextEmail.getText().toString(), editTextPassword.getText().toString());
             }
         });
 
+        // Listener for click and action for googleSignIn()
         buttonGoogleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Log.d("CIS3334", "Google login ");
                 googleSignIn();
             }
         });
 
+        // Listener for click and action for signOut()
         buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Log.d("CIS3334", "Logging out - signOut ");
                 signOut();
             }
         });
@@ -80,23 +93,84 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * method to create account from user/pass
+     * @param email String email address (no verification or scrub)
+     * @param password String untested password
+     */
     private void createAccount(String email, String password) {
+        // createUser method from Firebase instance, listen for return
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails
+                            //updateUI(null);
+                        }
 
+                        // ...
+                    }
+                });
     }
 
+    /**
+     * method for normal signin with existing account
+     * @param email String email address (no verification or scrub)
+     * @param password String untested password
+     */
     private void signIn(String email, String password){
-
+        // signin method from Firebase instance, listen for return
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        }
+                    }
+                });
     }
 
+    /**
+     * method for sign out of service
+     */
     private void signOut () {
-
+        // Firebase signOut method
+        mAuth.signOut();
     }
 
+    /**
+     * method for signin with google credentials (unimplemented)
+     */
     private void googleSignIn() {
-
+        throw new UnsupportedOperationException("TODO");
     }
 
-
-
+    /**
+     * implementation of onStart() method from super
+     * used to indicate login status with textView
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // Firebase method for returning current user value
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        if (currentUser != null) {
+            // User is signed in
+            textViewStatus.setText("Signed In");
+        } else {
+            // User is signed out
+            textViewStatus.setText("Signed Out");
+        }
+    }
 
 }
